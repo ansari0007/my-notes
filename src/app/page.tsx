64efa70a95230edc5fbc76a884b0e3b0a5db175c
Najm <nazmuddin@gamerpe.in>
2024-06-30 18:01:39 +0530

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">Notify App</span>
         <span className="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-purple-500 to-pink-500"></span>
     </h1>
-    <p className="text-lg text-gray-800 mb-8 text-center">Save your Notes here</p>
+    <p className="text-lg text-gray-800 mb-8 text-center">Save your Notes here(ci/cd)</p>
 
     </div>
   );
